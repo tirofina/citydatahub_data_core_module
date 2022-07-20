3c98416a2592afd69b23bb8a02f3fbb35fd90c69
leedevvv <lee.devvv@gmail.com>
2022-07-21 01:33:57 +0900

    fix. [Dashboard/UI] add scatter-history time option

--- dashboard/frontend/src/moudules/cookieParser.js
+++ dashboard/frontend/src/moudules/cookieParser.js
@@ -2,7 +2,6 @@ export const getCookie = (name) => {
   const cookies = document.cookie.split(';');
   for (const cookie of cookies) {
     const splited = cookie.split('=');
-    debugger;
     if (splited[0] === name) {
       return splited[1];
     }
